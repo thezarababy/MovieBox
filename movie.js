0af0c7c58cf9c6004cf6c10d4d02ce4b37184d65
thezarababy <jyokoduwa@gmail.com>
2025-06-14 10:52:35 +0100

    html and js only

---
+++ movie.js
@@ -0,0 +1,29 @@
+const movieDisplay = document.getElementById('movieDisplay');
+const browseMovie = document.getElementById('browseMovie');
+ async function fetchMovies(){
+  try{
+     movieDisplay.innerHTML="";
+     const response= await fetch("https://api.themoviedb.org/3/trending/movie/day?api_key=2594b313a8dd028dd39fd6439a4b84a7");
+     const data =await response.json();
+    
+  for(i=0; i<20; i++ ){
+    const movie = data.results[i];
+    const movieHolder = document.createElement("div");
+    const imgSrc= `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`;
+    movieHolder.innerHTML=`
+    <img src=" ${imgSrc}"/>
+    <h3> ${movie.title}</h3>
+    <p>${movie.overview}<p>`
+     movieDisplay.appendChild(movieHolder);
+  }
+   
+  }catch(error){
+    console.error("Error fetching movies:", error);
+    movieDisplay.textContent = "Failed to load movies.";
+  
+  }
+}
+ 
+
+
+browseMovie.addEventListener("click", fetchMovies);
